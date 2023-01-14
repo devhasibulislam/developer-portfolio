579f36fcddda2854bc685e1b369550938b155091
hasibulislam999 <hasib143sl@gmail.com>
2023-01-14 13:35:57 +0600

    social widget config init

---
+++ src/common/widgets/Socials/config/constans.tsx
@@ -0,0 +1,10 @@
+import { FiGithub, FiLinkedin, FiTwitter, FiFacebook } from "react-icons/fi";
+import { SocialTypes } from "./types";
+
+// social media data
+export const socialData: SocialTypes[] = [
+  { icon: FiGithub, url: "https://github.com/hasibulislam999" },
+  { icon: FiLinkedin, url: "https://www.linkedin.com/in/hasibulislam999" },
+  { icon: FiTwitter, url: "https://twitter.com/hasibulislam999" },
+  { icon: FiFacebook, url: "https://www.facebook.com/hasibulislam999.dev" },
+];
