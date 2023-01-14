237b5bcb4cde701dc36feccb68d2187f46bbaf9e
hasibulislam999 <hasib143sl@gmail.com>
2023-01-14 13:36:28 +0600

    config module init

---
+++ src/modules/Home/config/constanst.ts
@@ -0,0 +1,2 @@
+// skill list
+export const skills = [ "Redux", "Javascript","React js", "Next js", "Tailwind CSS", "Daisy UI", "Firebase", "Node js", "Express js", "MongoDB", "Socket.io", "graphGPT" ]
