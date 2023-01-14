6a5fb4864c20433dd837c36ea8ded620815bbe09
hasibulislam999 <hasib143sl@gmail.com>
2023-01-14 13:34:30 +0600

    portfolio meta description init

---
+++ src/common/components/meta/Meta.tsx
@@ -0,0 +1,35 @@
+const Meta = () => {
+  return (
+    <>
+      <link rel="icon" type="image/x-icon" href="/favicon.ico"></link>
+      <link rel="canonical" href="https://hasibulislam999.vercel.app/"></link>
+      <link rel="pingback" href="https://hasibulislam999.vercel.app/"></link>
+
+      <title> Hasibul Islam : Portfolio </title>
+
+      <meta name="description" content='Highly motivated and very passionate Full Stack Developer with three years of experience in Typescript, React js, Next js, Firebase, Tailwind CSS, Material UI, Node js, Express js, MongoDB' />
+      <meta
+        name="keywords"
+        content="Web Development Company, Best Web development agency, Web Development Services, Website Development Company, Website Development Services, Website Development Bangladesh, Web Development Bangladesh, Company, Services, Bangladesh, Flexsin, Flexsin Technologies"
+      />
+      
+      <meta name="google-site-verification" content="3yNZMVW_AuwCoLV0wk7WxYfo2hsZ4J8nVspHQQ5vg30" />
+      <meta property="og:site_name" content="Hasibul Islam" />
+      <meta property="og:title" content='Hasibul Islam | Portfolio' />
+      <meta property="og:description" content='Web Development Company, Best Web development agency, Web Development Services, Website Development Company, Website Development Services, Website Development Bangladesh, Web Development Bangladesh, Company, Services, Bangladesh, Flexsin, Flexsin Technologies' />
+      <meta property="og:url" content='https://hasibulislam999.vercel.app/' />
+      <meta property="og:image" content='https://res.cloudinary.com/dho0rpn5a/image/upload/v1672826380/canim/2cdd6f4e20b34196_HI06-copy.jpg.jpg' />
+      <meta property="og:type" content="website" />
+
+      <meta property="article:publisher" content="https://www.facebook.com/hasibulislam999.dev" />
+
+      <meta name="twitter:card" content="summary" />
+      <meta name="twitter:site" content="@hasibulislam999" />
+      <meta name="twitter:creator" content="@hasibulislam999" />
+      <meta name="twitter:description" content='Highly motivated and very passionate Full Stack Developer with three years of experience in Typescript, React js, Next js, Firebase, Tailwind CSS, Material UI, Node js, Express js, MongoDB' />
+      <meta name="twitter:image" content='https://res.cloudinary.com/dho0rpn5a/image/upload/v1672826380/canim/2cdd6f4e20b34196_HI06-copy.jpg.jpg' />
+    </>
+  );
+};
+
+export default Meta;
