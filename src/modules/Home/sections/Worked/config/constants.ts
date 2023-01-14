d68458d872c82b72572d3833ae12be8995b5e8e7
hasibulislam999 <hasib143sl@gmail.com>
2023-01-14 13:37:42 +0600

    worked config section init

---
+++ src/modules/Home/sections/Worked/config/constants.ts
@@ -0,0 +1,57 @@
+import { WorkedTypes } from "./types";
+
+// worked data
+export const worked_data: WorkedTypes[] = [
+  {
+    id: 0,
+    label: "Linkedin",
+    description: [
+      "Write modern, performant, maintainable code for a diverse array of client and internal projects",
+      "Work with a variety of different languages, platforms, frameworks, and content management systems such as JavaScript, TypeScript, Socket.io, Next js, React, WordPress, and Netlify",
+      "Communicate with multi-disciplinary teams of engineers, designers, producers, and clients on a daily basis",
+    ],
+    deadline: "March 22 - present",
+    title: "Full stack developer",
+    company: "Linkedin",
+    url: "https://www.linkedin.com/in/hasibulislam999",
+  },
+  {
+    id: 1,
+    label: "Fiverr",
+    description: [
+      "Write modern, performant, maintainable code for a diverse array of client and internal projects",
+      "Work with a variety of different languages, platforms, frameworks, and content management systems such as JavaScript, TypeScript, Socket.io, Next js, React, WordPress, and Netlify",
+      "Communicate with multi-disciplinary teams of engineers, designers, producers, and clients on a daily basis",
+    ],
+    deadline: "Aug 22 - present",
+    title: "Full stack developer",
+    company: "Fiverr",
+    url: "https://fiverr.com/hasibulislam999",
+  },
+  {
+    id: 2,
+    label: "Upwork",
+    description: [
+      "Write modern, performant, maintainable code for a diverse array of client and internal projects",
+      "Work with a variety of different languages, platforms, frameworks, and content management systems such as JavaScript, TypeScript, Socket.io, Next js, React, WordPress, and Netlify",
+      "Communicate with multi-disciplinary teams of engineers, designers, producers, and clients on a daily basis",
+    ],
+    deadline: "Aug 22 - present",
+    title: "Full stack developer",
+    company: "Upwork",
+    url: "https://www.upwork.com/freelancers/~010348caa6e1214162",
+  },
+  {
+    id: 2,
+    label: "Freelancer",
+    description: [
+      "Write modern, performant, maintainable code for a diverse array of client and internal projects",
+      "Work with a variety of different languages, platforms, frameworks, and content management systems such as JavaScript, TypeScript, Socket.io, Next js, React, WordPress, and Netlify",
+      "Communicate with multi-disciplinary teams of engineers, designers, producers, and clients on a daily basis",
+    ],
+    deadline: "Aug 22 - present",
+    title: "Full stack developer",
+    company: "Freelancer",
+    url: "https://www.freelancer.com/u/hasibulislam999",
+  },
+];
