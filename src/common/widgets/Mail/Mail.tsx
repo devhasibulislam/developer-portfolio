44102b6cbbbf9b7901438941d944c306c6e1f432
hasibulislam999 <hasib143sl@gmail.com>
2023-06-21 13:05:41 +0600

    email shift to new form old

--- src/common/widgets/Mail/Mail.tsx
+++ src/common/widgets/Mail/Mail.tsx
@@ -10,7 +10,7 @@ const Mail = () => {
   return (
     <motion.div variants={fadeTop} {...motionStep} id="__mail" className='fixed z-[999] bottom-0 right-[40px] hidden xl:block' >
         <div className='space-y-6 after:h-[90px] after:w-[1px] after:bg-slate-400 after:block after:mx-auto after:mt-6' >
-           <Link href='mailto:hasib143sl@gmail.com' className='text-[15px] text-slate-400 hover:text-sky-400 cursor-pointer transition-all duration-300 hover:translate-y-[-10px] rl' >hasib143sl@gmail.com</Link> 
+           <Link href='mailto:developer.hasibulislam@gmail.com' className='text-[15px] text-slate-400 hover:text-sky-400 cursor-pointer transition-all duration-300 hover:translate-y-[-10px] rl' >developer.hasibulislam@gmail.com</Link> 
         </div>
     </motion.div>
   )
