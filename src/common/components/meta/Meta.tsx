6bd5d6acb4a3153d705c1790951584434531db36
hasibulislam999 <hasib143sl@gmail.com>
2023-06-21 13:05:24 +0600

    meta keywords modify

--- src/common/components/meta/Meta.tsx
+++ src/common/components/meta/Meta.tsx
@@ -10,13 +10,13 @@ const Meta = () => {
       <meta name="description" content='Highly motivated and very passionate Full Stack Developer with three years of experience in Typescript, React js, Next js, Firebase, Tailwind CSS, Material UI, Node js, Express js, MongoDB' />
       <meta
         name="keywords"
-        content="Web Development Company, Best Web development agency, Web Development Services, Website Development Company, Website Development Services, Website Development Bangladesh, Web Development Bangladesh, Company, Services, Bangladesh, Flexsin, Flexsin Technologies"
+        content="Web Development Company, Best Web development agency, Web Development Services, Website Development Company, Website Development Services, Website Development Bangladesh, Web Development Bangladesh, Company, Services, Bangladesh, Lear Generation"
       />
       
       <meta name="google-site-verification" content="3yNZMVW_AuwCoLV0wk7WxYfo2hsZ4J8nVspHQQ5vg30" />
       <meta property="og:site_name" content="Hasibul Islam" />
-      <meta property="og:title" content='Hasibul Islam | Portfolio' />
-      <meta property="og:description" content='Web Development Company, Best Web development agency, Web Development Services, Website Development Company, Website Development Services, Website Development Bangladesh, Web Development Bangladesh, Company, Services, Bangladesh, Flexsin, Flexsin Technologies' />
+      <meta property="og:title" content='Hasibul Islam • Portfolio' />
+      <meta property="og:description" content='Web Development Company, Best Web development agency, Web Development Services, Website Development Company, Website Development Services, Website Development Bangladesh, Web Development Bangladesh, Company, Services, Bangladesh, Lear Generation' />
       <meta property="og:url" content='https://hasibulislam999.vercel.app/' />
       <meta property="og:image" content='https://res.cloudinary.com/dho0rpn5a/image/upload/v1687018950/samples/Social_Image_rd28bz.png' />
       <meta property="og:type" content="website" />
