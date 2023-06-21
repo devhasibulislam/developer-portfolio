c3c3e5d3dcc242572d7b9ccd524687ebdf5e0440
hasibulislam999 <hasib143sl@gmail.com>
2023-06-21 13:05:12 +0600

    airbnb clone project init

--- src/shared/config/constants.ts
+++ src/shared/config/constants.ts
@@ -10,134 +10,45 @@ export const navData = ["About", "Experience", "Works", "Contact"];
 // project data
 export const projects: ProjectTypes[] = [
   {
-    title: "nLog",
+    title: "Airbnb Clone",
     description:
-      "Democratic Platform to write anti-political stuffs. an informational website published on the World Wide Web consisting of discrete, often informal diary-style text entries (posts).",
+      "A clone of Airbnb website where you can book hotels, rooms, and other places to stay.",
+    tech: ["MERN", "Front-end", "Back-end"],
+    code: "https://github.com/developer-hasibulislam/airbnb-clone",
+    live: "https://airbnb-clone-blue-kappa.vercel.app/",
+    thumbnail: "airbnb-clone.png",
+    featured: true,
+  },
+
+  {
+    title: "nLog Blog",
+    description:
+      "A blog website which is a democratic platform to write anti-political stuffs. ",
     tech: ["MERN", "Front-end", "Back-end"],
     code: "https://github.com/hasibulislam999/9T5--nLog-blogs",
     live: "https://9t5-nlog-blogs-client.vercel.app/",
     thumbnail: "nLog.png",
-    featured: true,
+    featured: false,
   },
 
   {
     title: "X-Beat Store",
     description:
-      "Display products as slide-show. Cart each product and approach increment-decrement with checkout as demo. See product description. Filter all products based on suggested category.",
-    tech: ["React js", "SASS", "React Router", "React Swiper"],
+      "A music store website where you can buy musical instruments and accessories.",
+    tech: ["MERN", "Front-end", "Back-end"],
     code: "https://github.com/hasibulislam999/xBeat-stable",
     live: "https://xbeat-stable.netlify.app/",
     thumbnail: "x-beat.png",
-    featured: true,
+    featured: false,
   },
 
   {
     title: "Canim Blog",
     description:
-      "Real-time watches, likes and comments. Admin and user dashboard with CRUD prodigies. Authentication like login, registration and forgot password system",
+      "A blog website where you can read blogs, write blogs, comment on blogs, like blogs, and share blogs.",
     tech: ["MERN", "Front-end", "Back-end"],
     code: "https://github.com/hasibulislam999/Nurui",
     live: "https://nurui-csr.vercel.app/",
     thumbnail: "canim-blog.png",
-  },
-
-  {
-    title: "Canim Inventory",
-    description:
-      "Maintenance & patches are shutdown. A semi-complete inventory platform. Which convey functional features, to access panel must contact me.",
-    tech: ["MERN", "Front-end", "Back-end"],
-    code: "https://github.com/hasibulislam999/Canim",
-    live: "https://canim.vercel.app/",
-    thumbnail: "canim-store1.png",
-  },
-
-  {
-    title: "Canim Store",
-    description:
-      "Maintenance & patches are shutdown. A semi-complete e-commerce platform. Which convey functional features, to access panel must contact me.",
-    tech: ["MERN", "Front-end", "Back-end"],
-    code: "https://github.com/hasibulislam999/e-commerce-csr",
-    live: "https://e-commerce-csr.vercel.app/",
-    thumbnail: "canim-store2.png",
-  },
-
-  {
-    title: "Amazon Clone",
-    description:
-      "Integrated Auth0 authentication for login, registration and forgot password also Google sign in. Contain complete real-time carting system.",
-    tech: ["React js", "tailwind CSS", "Context API"],
-    code: "https://github.com/hasibulislam999/amazon-clone-simple",
-    live: "https://amazon-clone-simple.vercel.app/",
-    thumbnail: "amazon-clone.png",
-  },
-
-  {
-    title: "Portfolio",
-    description:
-      "Maintenance & patches are shutdown. But my previous work-flow still existing and responsive as go thereby. By the by you can contact me from there. Also collect my minimal projects.",
-    tech: ["Next js 13", "TypeScript", "React-Bootstrap 5"],
-    code: "https://github.com/hasibulislam999/Portfolio",
-    live: "https://pofo-portfolio.vercel.app/",
-    thumbnail: "old-portfolio.png",
-  },
-
-  {
-    title: "GP Business",
-    description:
-      "A consulting template carefully crafted for designer, artists, company, photographer, videographer, architect etc. Gp also could easily be adapted to suit a wide range of websites.",
-    tech: ["HTML5", "CSS3", "JQuery-CDN", "AOS"],
-    code: "https://github.com/hasibulislam999/powerful-static-business",
-    live: "https://hasibulislam999.github.io/powerful-static-business/",
-    thumbnail: "gp-business.png",
-  },
-
-  {
-    title: "The Event",
-    description:
-      "It's an event organizing or event inspiration website. Which can obey an event and represent it's sweet and angular options which although can retain the semantics of any location.",
-    tech: ["HTML5", "CSS3", "JQuery-CDN", "AOS"],
-    code: "https://github.com/hasibulislam999/demo-theEvent",
-    live: "https://hasibulislam999.github.io/demo-theEvent/",
-    thumbnail: "the-event.png",
-  },
-
-  {
-    title: "Moderna",
-    description:
-      "Office stuff entertainment solution based. That require stuffs information and gather them to analyze and finally reveal them weather it's static site just to illustrate the visions.",
-    tech: ["HTML5", "CSS3", "JQuery-CDN", "AOS"],
-    code: "https://github.com/hasibulislam999/demo-mordana",
-    live: "https://hasibulislam999.github.io/demo-mordana/",
-    thumbnail: "moderna.png",
-  },
-
-  {
-    title: "Bethany",
-    description:
-      "Tree based health-care solutions like tree medicines, tree shops, about tree comprehensive. Locality of tree warehouse and build connection with tree solution vendors.",
-    tech: ["HTML5", "CSS3", "JQuery-CDN", "AOS"],
-    code: "https://github.com/hasibulislam999/demo-bethany",
-    live: "https://hasibulislam999.github.io/demo-bethany/",
-    thumbnail: "bethany.png",
-  },
-
-  {
-    title: "Arsha",
-    description:
-      "Provide multi-types solutions in business to scale it as a next level concern. But this is a static website which radially used for display as a project but as taking ideas it's literally best.",
-    tech: ["HTML5", "CSS3", "JQuery-CDN", "AOS"],
-    code: "https://github.com/hasibulislam999/demo-arsha",
-    live: "https://hasibulislam999.github.io/demo-arsha/",
-    thumbnail: "arsha.png",
-  },
-
-  {
-    title: "Logo Tech",
-    description:
-      "Contain authentication like login, register and forgot password at real time. An UI design based landing page which contain some sections that relay or indicate about to something.",
-    tech: ["React js", "JavaScript", "Bootstrap 5"],
-    code: "https://github.com/hasibulislam999/logo-tech",
-    live: "https://logo-tech.vercel.app/",
-    thumbnail: "logo-tech.png",
   },
 ];
