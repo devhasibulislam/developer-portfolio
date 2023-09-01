991186ecc23a7eafcaeaf1379afccd124fa9a26c
devhasibulislam <developer.hasibulislam@gmail.com>
2023-09-01 16:29:57 +0600

    modify description & tags

--- src/shared/config/constants.ts
+++ src/shared/config/constants.ts
@@ -5,72 +5,147 @@ import { ProjectTypes } from "./types";
 export const cx = classNames;
 
 // nav data
-export const navData = ["About", "Experience", "Works", "Contact"];
+export const navData = ["About", "Social", "Works", "Contact"];
 
 // project data
 export const projects: ProjectTypes[] = [
   {
+    title: "PlanNao Template",
+    description:
+      "All you need is mentor' is a pioneering ed-tech platform in Bangladesh connecting students with mentors for personalized guidance and bridging the gap between education and success.",
+    tech: [
+      "Next.JS",
+      "Redux",
+      "Tailwind-CSS",
+      "Vercel",
+      "MongoDB",
+      "Cloudinary",
+      "Nodemailer",
+      "JWT",
+    ],
+    code: "https://github.com/devhasibulislam/plannao-template",
+    live: "https://plannao-template.vercel.app",
+    thumbnail: "plannao-template.png",
+    featured: true,
+  },
+
+  {
     title: "ML Prediction Dashboard",
     description:
-      "Complete admin dashboard for a machine learning prediction website where you can manage users, predictions, and more.",
-    tech: ["MERN", "Front-end", "Back-end"],
+      "A comprehensive admin dashboard for a machine learning prediction website, facilitating user and prediction management, among other functions.",
+    tech: [
+      "Vite",
+      "Redux",
+      "Vercel",
+      "MUI",
+      "MongoDB",
+      "Express.JS",
+      "Mongoose",
+      "Render",
+      "Morgan",
+    ],
     code: "https://github.com/devhasibulislam/finanseer-clone",
-    live: "https://finanseer-clone.vercel.app/",
+    live: "https://finanseer-clone.vercel.app",
     thumbnail: "finanseer-clone.png",
-    featured: true,
+    featured: false,
   },
 
   {
     title: "ECommerce Admin Dashboard",
     description:
-      "Complete admin dashboard for an ecommerce website where you can manage products, orders, customers, and more.",
-    tech: ["MERN", "Front-end", "Back-end"],
+      "Full-featured e-commerce admin dashboard: Efficiently oversee products, orders, customers, and additional functionalities for seamless website management.",
+    tech: [
+      "Next.JS",
+      "Redux",
+      "Vercel",
+      "MUI",
+      "Nivo",
+      "MongoDB",
+      "Express.JS",
+      "Render",
+      "Emotion",
+    ],
     code: "https://github.com/devhasibulislam/ecomvision-clone",
-    live: "https://ecomvision-clone-client.onrender.com/",
+    live: "https://ecomvision-clone-client.onrender.com",
     thumbnail: "ecomvision-clone.png",
-    featured: true,
+    featured: false,
   },
 
   {
     title: "Airbnb Clone",
     description:
-      "A clone of Airbnb website where you can book hotels, rooms, and other places to stay.",
-    tech: ["MERN", "Front-end", "Back-end"],
+      "A replicated Airbnb website offering bookings for various accommodations, including hotels, rooms, and other lodging options, mirroring the original platform's functionality and features.",
+    tech: [
+      "Next.JS",
+      "Next-Auth",
+      "Zustand",
+      "Cloudinary",
+      "Axios",
+      "Vercel",
+      "MongoDB",
+      "React-Icons",
+    ],
     code: "https://github.com/devhasibulislam/airbnb-clone",
-    live: "https://airbnb-clone-blue-kappa.vercel.app/",
+    live: "https://airbnb-clone-blue-kappa.vercel.app",
     thumbnail: "airbnb-clone.png",
-    featured: true,
+    featured: false,
   },
 
   {
-    title: "X-Beat Store",
+    title: "X-Beat Template",
     description:
-      "A music store website where you can buy musical instruments and accessories.",
-    tech: ["MERN", "Front-end", "Back-end"],
-    code: "https://github.com/hasibulislam999/xBeat-stable",
-    live: "https://xbeat-stable.netlify.app/",
+      "E-commerce platform specializing in musical instruments and accessories, offering a wide range of products for purchase on their website.",
+    tech: [
+      "React.JS",
+      "React-Router-Dom",
+      "React-Icons",
+      "Swiper",
+      "EsLint",
+      "Sass",
+      "Search-Filter",
+    ],
+    code: "https://github.com/devhasibulislam/xBeat-stable",
+    live: "https://xbeat-stable.netlify.app",
     thumbnail: "x-beat.png",
     featured: false,
   },
 
   {
-    title: "nLog Blog",
+    title: "nLog Blog Template",
     description:
-      "A blog website which is a democratic platform to write anti-political stuffs. ",
-    tech: ["MERN", "Front-end", "Back-end"],
-    code: "https://github.com/hasibulislam999/9T5--nLog-blogs",
-    live: "https://9t5-nlog-blogs-client.vercel.app/",
+      "A democratic blog platform where users can freely express anti-political views and opinions, fostering open discourse and discussion.",
+    tech: [
+      "React.JS",
+      "Hapi/Joi",
+      "Morgan",
+      "Express.JS",
+      "MongoDB",
+      "Axios",
+      "DayJS",
+      "Mongoose",
+    ],
+    code: "https://github.com/devhasibulislam/9T5--nLog-blogs",
+    live: "https://9t5-nlog-blogs-client.vercel.app",
     thumbnail: "nLog.png",
     featured: false,
   },
 
   {
-    title: "Canim Blog",
+    title: "Canim Blog Template",
     description:
-      "A blog website where you can read blogs, write blogs, comment on blogs, like blogs, and share blogs.",
-    tech: ["MERN", "Front-end", "Back-end"],
-    code: "https://github.com/hasibulislam999/Nurui",
-    live: "https://nurui-csr.vercel.app/",
+      "A blog platform offering reading, writing, commenting, liking, and sharing functionalities for users to engage with and create content.",
+    tech: [
+      "React.JS",
+      "Swiper",
+      "Express.JS",
+      "Cloudinary",
+      "Helmet",
+      "Redux",
+      "MongoDB",
+      "Mongoose",
+    ],
+    code: "https://github.com/devhasibulislam/Nurui",
+    live: "https://nurui-csr.vercel.app",
     thumbnail: "canim-blog.png",
   },
 ];
