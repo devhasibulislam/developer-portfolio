5c1e2886921b4abc0216b838f2badd5adb26cae4
devhasibulislam <developer.hasibulislam@gmail.com>
2023-09-20 23:38:03 +0600

    integrate top level header

--- src/modules/Home/Home.tsx
+++ src/modules/Home/Home.tsx
@@ -1,26 +1,30 @@
-import React from 'react'
+import React from "react";
 //
-import About from './sections/About'
-import GetInTouch from './sections/GetInTouch'
-import Hero from './sections/Hero'
-import Projects from './sections/Projects'
-import Worked from './sections/Worked'
-import Footer from '@widgets/Footer'
-
+import About from "./sections/About";
+import GetInTouch from "./sections/GetInTouch";
+import Hero from "./sections/Hero";
+import Projects from "./sections/Projects";
+import Worked from "./sections/Worked";
+import Footer from "@widgets/Footer";
+import Head from "next/head";
 
 const Home = () => {
   return (
     <>
-
+      <Head>
+        <title>
+          Hasibul Islam, an experienced MERN Stack Developer and Lead Generation
+          Expert!
+        </title>
+      </Head>
       <Hero />
       <About />
       <Worked />
       <Projects />
       <GetInTouch />
       <Footer />
-
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
