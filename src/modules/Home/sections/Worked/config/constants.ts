3f84b3d3247cf78874b558d255a0ee7c91e46ecd
devhasibulislam <developer.hasibulislam@gmail.com>
2023-11-22 12:56:48 +0600

    project constants integrated

--- src/modules/Home/sections/Worked/config/constants.ts
+++ src/modules/Home/sections/Worked/config/constants.ts
@@ -1,131 +1,44 @@
-import { WorkedTypes } from "./types";
+import { WorkedTypes } from './types';
 
 // worked data
-export const worked_data: WorkedTypes[] = [
-  {
-    id: 0,
-    label: "Linkedin",
-    description: [
-      "Experienced Full Stack Developer || Lead Generation Expert",
-      "Hasibul Islam is a MERN Stack developer and Lead Generation expert driving web app development and revenue growth.",
-      "Full Stack Development using MREN Stack & Lear Generation Expert",
-      "Lead Generation, JavaScript, MongoDB, React.js, Node.js, Git, GitHub, Front-End Development, Back-End Development, REST APIs, SEO, Linux",
-      "B.Sc in CSE at City University of Bangladesh",
-    ],
-    deadline: "December 2020 - Present",
-    title: "Full stack developer",
-    company: "devhasibulislam",
-    url: "https://www.linkedin.com/in/devhasibulislam",
-  },
-
-  {
-    id: 1,
-    label: "GitHub",
-    description: [
-      "Experienced Full Stack Developer || Lead Generation Expert",
-      "Hasibul Islam is a MERN Stack developer and Lead Generation expert driving web app development and revenue growth.",
-      "Full Stack Development using MREN Stack & Lear Generation Expert",
-      "Lead Generation, JavaScript, MongoDB, React.js, Node.js, Git, GitHub, Front-End Development, Back-End Development, REST APIs, SEO, Linux",
-      "B.Sc in CSE at City University of Bangladesh",
-    ],
-    deadline: "December 2020 - Present",
-    title: "Full stack developer",
-    company: "devhasibulislam",
-    url: "https://github.com/devhasibulislam",
-  },
-  
-  {
-    id: 2,
-    label: "Facebook",
-    description: [
-      "Experienced Full Stack Developer || Lead Generation Expert",
-      "Hasibul Islam is a MERN Stack developer and Lead Generation expert driving web app development and revenue growth.",
-      "Full Stack Development using MREN Stack & Lear Generation Expert",
-      "Lead Generation, JavaScript, MongoDB, React.js, Node.js, Git, GitHub, Front-End Development, Back-End Development, REST APIs, SEO, Linux",
-      "B.Sc in CSE at City University of Bangladesh",
-    ],
-    deadline: "December 2020 - Present",
-    title: "Full stack developer",
-    company: "devhasibulislam",
-    url: "https://facebook.com/devhasibulislam",
-  },
-  
-  {
-    id: 3,
-    label: "Instagram",
-    description: [
-      "Experienced Full Stack Developer || Lead Generation Expert",
-      "Hasibul Islam is a MERN Stack developer and Lead Generation expert driving web app development and revenue growth.",
-      "Full Stack Development using MREN Stack & Lear Generation Expert",
-      "Lead Generation, JavaScript, MongoDB, React.js, Node.js, Git, GitHub, Front-End Development, Back-End Development, REST APIs, SEO, Linux",
-      "B.Sc in CSE at City University of Bangladesh",
-    ],
-    deadline: "December 2020 - Present",
-    title: "Full stack developer",
-    company: "devhasibulislam",
-    url: "https://instagram.com/devhasibulislam",
-  },
-  
-  {
-    id: 4,
-    label: "Twitter",
-    description: [
-      "Experienced Full Stack Developer || Lead Generation Expert",
-      "Hasibul Islam is a MERN Stack developer and Lead Generation expert driving web app development and revenue growth.",
-      "Full Stack Development using MREN Stack & Lear Generation Expert",
-      "Lead Generation, JavaScript, MongoDB, React.js, Node.js, Git, GitHub, Front-End Development, Back-End Development, REST APIs, SEO, Linux",
-      "B.Sc in CSE at City University of Bangladesh",
-    ],
-    deadline: "December 2020 - Present",
-    title: "Full stack developer",
-    company: "devhasibulislam",
-    url: "https://twitter.com/devhasibulislam",
-  },
-  
-  {
-    id: 5,
-    label: "Pinterest",
-    description: [
-      "Experienced Full Stack Developer || Lead Generation Expert",
-      "Hasibul Islam is a MERN Stack developer and Lead Generation expert driving web app development and revenue growth.",
-      "Full Stack Development using MREN Stack & Lear Generation Expert",
-      "Lead Generation, JavaScript, MongoDB, React.js, Node.js, Git, GitHub, Front-End Development, Back-End Development, REST APIs, SEO, Linux",
-      "B.Sc in CSE at City University of Bangladesh",
-    ],
-    deadline: "December 2020 - Present",
-    title: "Full stack developer",
-    company: "devhasibulislam",
-    url: "https://pinterest.com/devhasibulislam",
-  },
-
-  {
-    id: 6,
-    label: "WhatsApp",
-    description: [
-      "Experienced Full Stack Developer || Lead Generation Expert",
-      "Hasibul Islam is a MERN Stack developer and Lead Generation expert driving web app development and revenue growth.",
-      "Full Stack Development using MREN Stack & Lear Generation Expert",
-      "Lead Generation, JavaScript, MongoDB, React.js, Node.js, Git, GitHub, Front-End Development, Back-End Development, REST APIs, SEO, Linux",
-      "B.Sc in CSE at City University of Bangladesh",
-    ],
-    deadline: "December 2020 - Present",
-    title: "Full stack developer",
-    company: "+8801906-315901",
-    url: "https://wa.me/01906315901",
-  },
-  {
-    id: 7,
-    label: "Telegram",
-    description: [
-      "Experienced Full Stack Developer || Lead Generation Expert",
-      "Hasibul Islam is a MERN Stack developer and Lead Generation expert driving web app development and revenue growth.",
-      "Full Stack Development using MREN Stack & Lear Generation Expert",
-      "Lead Generation, JavaScript, MongoDB, React.js, Node.js, Git, GitHub, Front-End Development, Back-End Development, REST APIs, SEO, Linux",
-      "B.Sc in CSE at City University of Bangladesh",
-    ],
-    deadline: "December 2020 - Present",
-    title: "Full stack developer",
-    company: "devhasibulislam",
-    url: "https://t.me/devhasibulislam",
-  },
-];
+export const worked_data : WorkedTypes[] = [
+   { 
+      id: 0,
+      label : "Opedia Tech",
+      description : [
+        "Write modern, performant, maintainable code for a diverse array of client and internal projects",
+        "Work with a variety of different languages, platforms, frameworks, and content management systems such as JavaScript, TypeScript, Socket.io, Next js, React, WordPress, and Netlify",
+        "Communicate with multi-disciplinary teams of engineers, designers, producers, and clients on a daily basis",
+      ],
+      deadline : "December 2023 - Present",
+      title: "Jr. Web Developer",
+      company : "Opedia Technology",
+      url : "https://opediatech.com",
+   },
+   { 
+      id: 1,
+      label : "Fiverr",
+      description : [
+        "Write modern, performant, maintainable code for a diverse array of client and internal projects",
+        "Work with a variety of different languages, platforms, frameworks, and content management systems such as JavaScript, TypeScript, Socket.io, Next js, React, WordPress, and Netlify",
+        "Communicate with multi-disciplinary teams of engineers, designers, producers, and clients on a daily basis",
+      ],
+      deadline : "Aug 21 - Present",
+      title: "MERN Stack Developer",
+      company : "Fiverr",
+      url : "https://fiverr.com/devhasibulislam",
+   },
+   { 
+      id: 2,
+      label : "Upwork",
+      description : [
+        "Write modern, performant, maintainable code for a diverse array of client and internal projects",
+        "Work with a variety of different languages, platforms, frameworks, and content management systems such as JavaScript, TypeScript, Socket.io, Next js, React, WordPress, and Netlify",
+        "Communicate with multi-disciplinary teams of engineers, designers, producers, and clients on a daily basis",
+      ],
+      deadline : "Aug 22 - Present",
+      title: "Full Stack Developer",
+      company : "Upwork",
+      url : "https://www.upwork.com/freelancers/~01a3b8fc1dc09fedf5",
+   },
+]
