74319d9f3b3e0f57095c8e6ba70542b79dfc367e
devhasibulislam <developer.hasibulislam@gmail.com>
2023-11-22 17:19:21 +0600

    integrated year zone

--- src/modules/Home/sections/Worked/config/constants.ts
+++ src/modules/Home/sections/Worked/config/constants.ts
@@ -4,7 +4,7 @@ import { WorkedTypes } from './types';
 export const worked_data : WorkedTypes[] = [
    { 
       id: 0,
-      label : "Opedia Tech",
+      label : "Opedia Tech.",
       description : [
         "Write modern, performant, maintainable code for a diverse array of client and internal projects",
         "Work with a variety of different languages, platforms, frameworks, and content management systems such as JavaScript, TypeScript, Socket.io, Next js, React, WordPress, and Netlify",
@@ -17,26 +17,26 @@ export const worked_data : WorkedTypes[] = [
    },
    { 
       id: 1,
-      label : "Fiverr",
+      label : "Fiverr Inc.",
       description : [
         "Write modern, performant, maintainable code for a diverse array of client and internal projects",
         "Work with a variety of different languages, platforms, frameworks, and content management systems such as JavaScript, TypeScript, Socket.io, Next js, React, WordPress, and Netlify",
         "Communicate with multi-disciplinary teams of engineers, designers, producers, and clients on a daily basis",
       ],
-      deadline : "Aug 21 - Present",
+      deadline : "Aug 2021 - Present",
       title: "MERN Stack Developer",
       company : "Fiverr",
       url : "https://fiverr.com/devhasibulislam",
    },
    { 
       id: 2,
-      label : "Upwork",
+      label : "Upwork Inc.",
       description : [
         "Write modern, performant, maintainable code for a diverse array of client and internal projects",
         "Work with a variety of different languages, platforms, frameworks, and content management systems such as JavaScript, TypeScript, Socket.io, Next js, React, WordPress, and Netlify",
         "Communicate with multi-disciplinary teams of engineers, designers, producers, and clients on a daily basis",
       ],
-      deadline : "Aug 22 - Present",
+      deadline : "Aug 2022 - Present",
       title: "Full Stack Developer",
       company : "Upwork",
       url : "https://www.upwork.com/freelancers/~01a3b8fc1dc09fedf5",
