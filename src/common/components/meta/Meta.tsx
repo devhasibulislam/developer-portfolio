2fd67db2258c02678eca9f7e2e4b4d039aab76a7
devhasibulislam <developer.hasibulislam@gmail.com>
2024-01-03 20:02:46 +0600

    modify meta information

--- src/common/components/meta/Meta.tsx
+++ src/common/components/meta/Meta.tsx
@@ -3,6 +3,88 @@ import React from "react";
 const Meta = () => {
   return (
     <>
+      {/* Open Graph Tags (for Facebook) */}
+      <meta property="og:site_name" content="Hasibul Islam" />
+      <meta property="og:url" content="https://devhasibulislam.vercel.app/" />
+      <meta property="og:type" content="website" />
+      <meta
+        property="og:title"
+        content="Hasibul Islam (@devhasibulislam), a full stack web developer"
+      />
+      <meta
+        property="og:description"
+        content="#MernStack #FullStack #WebsiteDevelopment - Developing websites for individuals, startups, small and medium businesses, and enterprises"
+      />
+      <meta
+        property="og:image"
+        content="https://github.com/devhasibulislam/developer-portfolio/blob/v3/public/images/og/devhasibulislam%20-%20portfolio.png?raw=true"
+      />
+
+      {/* General Metadata */}
+      <meta name="author" content="Hasibul Islam" />
+      <title>
+        Hasibul Islam (@devhasibulislam), a full stack web developer
+      </title>
+      <meta name="robots" content="index,follow" />
+      <meta
+        title="Hasibul Islam (@devhasibulislam), a full stack web developer"
+        name="title"
+      />
+
+      {/* Description and Keywords Metadata */}
+      <meta
+        name="description"
+        content="#MernStack #FullStack #WebsiteDevelopment - Developing websites for individuals, startups, small and medium businesses, and enterprises"
+      />
+      <meta
+        name="keywords"
+        content="javascript, typescript, react js, next js, express js, node js, mongodb, mern stack developer, web developer, website developer"
+      />
+
+      <meta
+        name="google-site-verification"
+        content="sZPmFgCCG9yDDjVqCsdembcfkOLDihLkjCWdXkxnEVI"
+      />
+      <meta name="msvalidate.01" content="100EE9C6C0FC17AF45101A46F9363D0C" />
+      <meta name="p:domain_verify" content="c55db76c3a2ff1b5533315270d4676b3" />
+
+      {/* Article Publisher Metadata */}
+      <meta
+        property="article:publisher"
+        content="https://www.linkedin.com/in/devhasibulislam/"
+      />
+
+      {/* Twitter Card Tags */}
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:site" content="@devhasibulislam" />
+      <meta
+        name="twitter:title"
+        content="Hasibul Islam (@devhasibulislam), a full stack web developer"
+      />
+      <meta
+        name="twitter:description"
+        content="#MernStack #FullStack #WebsiteDevelopment - Developing websites for individuals, startups, small and medium businesses, and enterprises"
+      />
+      <meta
+        name="twitter:image"
+        content="https://github.com/devhasibulislam/developer-portfolio/blob/v3/public/images/og/devhasibulislam%20-%20portfolio.png?raw=true"
+      />
+
+      {/* Pinterest Tags */}
+      <meta name="pinterest-rich-pin" content="true" />
+      <meta
+        name="pinterest:title"
+        content="Hasibul Islam (@devhasibulislam), a full stack web developer"
+      />
+      <meta
+        name="pinterest:description"
+        content="#MernStack #FullStack #WebsiteDevelopment - Developing websites for individuals, startups, small and medium businesses, and enterprises"
+      />
+      <meta
+        name="pinterest:image"
+        content="https://github.com/devhasibulislam/developer-portfolio/blob/v3/public/images/og/devhasibulislam%20-%20portfolio.png?raw=true"
+      />
+
       {/* Favicons */}
       <link
         rel="apple-touch-icon"
@@ -24,87 +106,6 @@ const Meta = () => {
       <link rel="manifest" href="/site.webmanifest" />
       <link rel="canonical" href="https://devhasibulislam.vercel.app/"></link>
       <link rel="pingback" href="https://devhasibulislam.vercel.app/"></link>
-
-      {/* General Metadata */}
-      <meta name="author" content="Hasibul Islam" />
-      <title>
-        Hasibul Islam (@devhasibulislam), a full stack web developer
-      </title>
-      <meta
-        title="Hasibul Islam (@devhasibulislam), a full stack web developer"
-        name="title"
-      />
-
-      {/* Description and Keywords Metadata */}
-      <meta
-        name="description"
-        content="#MernStack #FullStack #WebsiteDevelopment - Developing websites for individuals, startups, small and medium businesses, and enterprises"
-      />
-      <meta
-        name="keywords"
-        content="javascript, typescript, react js, next js, express js, node js, mongodb, mern stack developer, web development, website development, web developer, website developer"
-      />
-
-      <meta
-        name="google-site-verification"
-        content="sZPmFgCCG9yDDjVqCsdembcfkOLDihLkjCWdXkxnEVI"
-      />
-      <meta name="msvalidate.01" content="100EE9C6C0FC17AF45101A46F9363D0C" />
-      <meta name="p:domain_verify" content="c55db76c3a2ff1b5533315270d4676b3" />
-
-      {/* Open Graph Tags (for Facebook) */}
-      <meta property="og:site_name" content="Hasibul Islam" />
-      <meta property="og:url" content="https://devhasibulislam.vercel.app/" />
-      <meta property="og:type" content="website" />
-      <meta
-        property="og:title"
-        content="Hasibul Islam (@devhasibulislam), a full stack web developer"
-      />
-      <meta
-        property="og:description"
-        content="#MernStack #FullStack #WebsiteDevelopment - Developing websites for individuals, startups, small and medium businesses, and enterprises"
-      />
-      <meta
-        property="og:image"
-        content="https://res.cloudinary.com/dkmf4t55r/image/upload/v1693567142/Og-Images/devhasibulislam.png"
-      />
-
-      {/* Article Publisher Metadata */}
-      <meta
-        property="article:publisher"
-        content="https://www.linkedin.com/in/devhasibulislam/"
-      />
-
-      {/* Twitter Card Tags */}
-      <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:site" content="@devhasibulislam" />
-      <meta
-        name="twitter:title"
-        content="Hasibul Islam (@devhasibulislam), a full stack web developer"
-      />
-      <meta
-        name="twitter:description"
-        content="#MernStack #FullStack #WebsiteDevelopment - Developing websites for individuals, startups, small and medium businesses, and enterprises"
-      />
-      <meta
-        name="twitter:image"
-        content="https://res.cloudinary.com/dkmf4t55r/image/upload/v1693567142/Og-Images/devhasibulislam.png"
-      />
-
-      {/* Pinterest Tags */}
-      <meta name="pinterest-rich-pin" content="true" />
-      <meta
-        name="pinterest:title"
-        content="Hasibul Islam (@devhasibulislam), a full stack web developer"
-      />
-      <meta
-        name="pinterest:description"
-        content="#MernStack #FullStack #WebsiteDevelopment - Developing websites for individuals, startups, small and medium businesses, and enterprises"
-      />
-      <meta
-        name="pinterest:image"
-        content="https://res.cloudinary.com/dkmf4t55r/image/upload/v1693567142/Og-Images/devhasibulislam.png"
-      />
     </>
   );
 };
