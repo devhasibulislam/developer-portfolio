5afd1ca77a3583d5b629e4fac42bb0080963b69c
devhasibulislam <developer.hasibulislam@gmail.com>
2024-01-30 11:38:43 +0600

    modify footer as free service

--- src/common/widgets/Footer/Footer.tsx
+++ src/common/widgets/Footer/Footer.tsx
@@ -5,17 +5,22 @@ const Footer = () => {
     <section id="__footer" className="py-5">
       <div className="text-center text-sm text-slate-400">
         <p className="opacity-90">
-          {" "}
-          Built & Designed by{" "}
+          Get 15 Minutes{" "}
+          <a
+            href="https://calendly.com/devhasibulislam/15min"
+            target="_blank"
+            className="text-sky-400"
+          >
+            Free Consultation
+          </a>.{" "}
+          Regards{" "}
           <a
             href="https://bento.me/devhasibulislam"
             target="_blank"
             className="text-sky-400"
           >
-            Hasibul Islam
-          </a>{" "}
-          & inspired from <span className="text-sky-400">Brittany Chiang</span>{" "}
-          ❤️{" "}
+            Hasibul Islam.
+          </a>
         </p>
       </div>
     </section>
